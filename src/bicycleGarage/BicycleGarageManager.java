6ef6ea78994ff180a84548bcbdeaeac1101469c3
Melldore <erik.j.mellberg@gmail.com>
2015-05-03 20:57:26 +0200

    Terminal timer

--- src/bicycleGarage/BicycleGarageManager.java
+++ src/bicycleGarage/BicycleGarageManager.java
@@ -1,12 +1,21 @@
 package bicycleGarage;
 
+import java.time.Instant;
+import java.util.Timer;
+
 import interfaces.*;
 
 public class BicycleGarageManager {
 	
 	BarcodePrinter printer;
+	ElectronicLock entryLock;
+	ElectronicLock exitLock;
+	PinCodeTerminal terminal;
+	StringBuilder code;
+	Instant firstChar;
 
 	public BicycleGarageManager() {
+		code = new StringBuilder();
 		
 	}
 
@@ -14,10 +23,26 @@ public class BicycleGarageManager {
 			ElectronicLock entryLock, ElectronicLock exitLock,
 			PinCodeTerminal terminal) {
 		this.printer = printer;
+		this.entryLock = entryLock;
+		this.exitLock = exitLock;
+		this.terminal = terminal;
 	}
 
 	public void entryCharacter(char c) {
-		System.out.println(c);
+		if(code.length() == 0) {
+			firstChar = Instant.now();
+		} else if (firstChar.isBefore(Instant.now().minusSeconds(5))){
+			firstChar = Instant.now();
+			code = new StringBuilder();
+			System.out.println("Too slow");
+			terminal.lightLED(0, 2);
+		}
+		code.append(c);
+		
+		if(code.length() == 4){
+			System.out.println(code.toString()); //Jämför med databas, om pinkod finns öppnas entryLock
+			code = new StringBuilder();
+		}
 	}
 
 	public void exitBarcode(String code) {
@@ -28,4 +53,5 @@ public class BicycleGarageManager {
 	public void entryBarcode(String code) {
 		printer.printBarcode(code);
 	}
+	
 }
