d53c306ad8d3abcac2775ad8d44ad448c9c22b73
RichardMagnusson <richard.pe.magnusson@gmail.com>
2015-05-11 15:06:50 +0200

    Create EditOperatorButton.java

---
+++ src/bicycleGarage/EditOperatorButton.java
@@ -0,0 +1,17 @@
+package bicycleGarage;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+
+public class EditOperatorButton extends JButton implements ActionListener{
+
+	public EditOperatorButton(){
+		super("Redigera Operatörskonto");
+		addActionListener(this);
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		
+	}
+}
