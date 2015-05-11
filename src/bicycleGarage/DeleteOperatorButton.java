6835272bba50a3f5a2272c83f013e8c6197480b7
RichardMagnusson <richard.pe.magnusson@gmail.com>
2015-05-11 15:05:38 +0200

    Create DeleteOperatorButton.java

---
+++ src/bicycleGarage/DeleteOperatorButton.java
@@ -0,0 +1,17 @@
+package bicycleGarage;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+
+public class DeleteOperatorButton extends JButton implements ActionListener{
+
+	public DeleteOperatorButton(){
+		super("Ta bort Operatörskonto");
+		addActionListener(this);
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		
+	}
+}
