8c3af4bd6bcf97469f6edbba29fcb2f78a7fe7bc
RichardMagnusson <richard.pe.magnusson@gmail.com>
2015-05-11 15:08:16 +0200

    Create ListUsersButton.java

---
+++ src/bicycleGarage/ListUsersButton.java
@@ -0,0 +1,17 @@
+package bicycleGarage;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+
+public class ListUsersButton extends JButton implements ActionListener{
+
+	public ListUsersButton(){
+		super("Lista Användare och Cyklar");
+		addActionListener(this);
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		
+	}
+}
